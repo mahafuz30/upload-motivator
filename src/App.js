167bc6685155c2c8b90fd01305ebf89d2a4bb620
Mahafuz Zaman <mahafuzzaman@H6V0HMX4L0.local>
2022-07-26 14:06:37 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import {Navbar,Container, Nav, Button} from 'react-bootstrap'
+import MeeshoLogo from '../Images/Meesho.png'
 
 export default class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <Navbar fixed='top' expand="lg">
+        <Container>
+          <Navbar.Brand href='#' style={{marginRight:"auto"}}>
+            <img
+            src={MeeshoLogo}
+            height="30"
+            alt="Meesho LOGO"
+            />
+          </Navbar.Brand>
+
+          <Button variant='light' href='#'>
+            Add Product
+          </Button>
+
+        </Container>
+      </Navbar>
     )
   }
 }
