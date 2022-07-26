a56e2ab465d4c13bdd725a5defe73dfb901ee2fc
Mahafuz Zaman <mahafuzzaman@H6V0HMX4L0.local>
2022-07-26 13:12:57 +0530

    Setup-project

---
+++ src/pages/ProductCatalog.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ProductCatalog extends Component {
+  render() {
+    return (
+      <div>ProductCatalog</div>
+    )
+  }
+}
---
+++ src/pages/ProductDescForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ProductDescForm extends Component {
+  render() {
+    return (
+      <div>ProductDescForm</div>
+    )
+  }
+}
