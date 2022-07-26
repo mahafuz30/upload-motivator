79eebd9e319ddb8c1ef9cef1cfd67b3e2969595d
patwa007 <101331715+patwa007@users.noreply.github.com>
2022-07-26 18:07:42 +0530

    updated dummy form

--- src/Utils/DummyFormData.js
+++ src/Utils/DummyFormData.js
@@ -8,51 +8,196 @@ const PriceSizeInventory = [
     {
         name : 'Wrong/Defective Returns Price',
         require : true,
-        desc : 'Customers buying at this price can only return wrong/defective delivered products.'
+        desc : 'Customers buying at this price can only return wrong/defective delivered products.',
+        type:"text"
     },
     {
         name : 'MRP',
         require : true,
-        desc : ""
+        desc : "",
+        type:"number"
     },
     {
         name : 'GST',
         require : true,
-        desc : ""
+        desc : "",
+        type:"number"
     },
     {
         name : 'HSN Code',
         require : true,
-        desc : ""
+        desc : "",
+        type:"text"
     },
     {
         name : 'Style code/ Product ID (optional)',
         require : false,
-        desc : ''
+        desc : '',
+        type:"number"
     },
     {
         name : 'Product Name',
         require : true,
-        desc : ''
+        desc : '',
+        type:"text"
     },
     {
         name : 'Size',
         require : true,
-        desc : ''
+        desc : '',
+        type:"text"
     },
 
 ]
 
-// For Select 
-const select = {
-    name : 'Size',
-    require : true,
-    desc : '',
-    option:[
-        'a',
-        'b',
-        'c'
-    ]
-}
+
+const ProductDetails = [
+    {
+        name : "Color",
+        require :true,
+        desc : '',
+        type : "select",
+        option : ['Red','Green', 'Blue']
+    },
+    {
+        name : "Multipack",
+        require : true,
+        desc :'',
+        type :"select",
+        option :['Pack of 2', 'Pack of 5']
+    }
+]
+
+const OtherAttributes = [
+    {
+        name : "Blouse",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['Running Blouse', "Without Blouse"]
+    },
+    {
+        name : "Blouse Color",
+        require :false,
+        desc :'',
+        type :"select",
+        option :["Black", "Blue"]
+    },
+    {
+        name : "Blouse Fabric",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['Acrylic', 'Art Silk']
+    },
+    {
+        name : "Blouse Length (mtrs)",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['0.5', '0.6']
+    },
+    {
+        name : "Blouse Pattern",
+        require :false,
+        desc :"",
+        type :"select",
+        option :['Solid','Zari Woven']
+    },
+    {
+        name : "Border",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['No Border', 'Printed']
+    },
+    {
+        name :"Occasion",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['Daily', 'Party']
+    },
+    {
+        name :"Pallu Details",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['Embellished','Embroidered']
+    },
+    {
+        name :"Pattern",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['Applique', 'Checked']
+    },
+    // {
+    //     name :"Display Test Print or Pattern Type",
+    //     require :false,
+    //     desc :'',
+    //     type :"select",
+    //     option :['']
+    // },
+    // {
+    //     name : "Saree Fabric",
+    //     require :false,
+    //     desc :'',
+    //     type :"select",
+    //     option :[]
+    // },
+    // {
+    //     name : "Saree Length (mtrs)",
+    //     require :false,
+    //     desc :'',
+    //     type :"select",
+    //     option :[]
+    // },
+    // {
+    //     name : "Transparency",
+    //     require :false,
+    //     desc :'',
+    //     type :"select",
+    //     option :[]
+    // },
+    // {
+    //     name :"Type",
+    //     require :false,
+    //     desc : '',
+    //     type :"select",
+    //     option :[]
+    // },
+    {
+        name : "COUNTRY OF ORIGIN",
+        require :false,
+        desc :'',
+        type :"select",
+        option :['India', "Bangladesh"]
+    },
+    {
+        name :"Description",
+        require :false,
+        desc :'',
+        type :"text"
+    },
+    {
+        name : "Manufacture Details",
+        require :false,
+        desc :'',
+        type :'text'
+    },
+    {
+        name :"Packer Details",
+        require :false,
+        desc :'',
+        type :"text"
+    },
+    {
+        name : "Importer Details",
+        require :false,
+        desc :'',
+        type :"text"
+    }
+]
 
 export {PriceSizeInventory};
