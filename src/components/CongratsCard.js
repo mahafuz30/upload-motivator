d3e759ff376333305db6ed0db3c0174311dee661
Shivam Chandak <shivamchandak@H7FDW3XDCR.local>
2022-07-27 12:34:32 +0530

    anim

--- src/components/CongratsCard.js
+++ src/components/CongratsCard.js
@@ -2,9 +2,14 @@ import Progress from './ProgressBar';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { FaCoins } from 'react-icons/fa';
+import Confetti from 'react-confetti';
+
 
 function CongratsCard() {
+
   return (
+    <>
+    <Confetti width={1400} height={850} /> 
     <Card className="text-center" style={{marginTop:'200px'}} >
       <Card.Header style={{color:'goldenrod', fontSize:'250%'}}>Awesome</Card.Header>
       <Card.Body>
@@ -17,6 +22,7 @@ function CongratsCard() {
       </Card.Body>
       <Card.Footer className="text-muted">Keep uploading to get more rewards</Card.Footer>
     </Card>
+    </>
   );
 }
 
