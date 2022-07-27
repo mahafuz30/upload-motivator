4b6b0af204245e2775b77d0f5ff81a5cfce39ced
Shivam Chandak <shivamchandak@H7FDW3XDCR.local>
2022-07-27 12:10:45 +0530

    umnag update

--- src/components/ReminderModal.js
+++ src/components/ReminderModal.js
@@ -1,18 +1,18 @@
-import React, { Component, useState  } from 'react'
+import React, { Component, useState } from 'react'
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { FaDna, FaImages } from "react-icons/fa";
+import { FaDna, FaImages, FaCoins } from "react-icons/fa";
 import Progress from './ProgressBar';
-import { Link , useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 
 
 function MyVerticallyCenteredModal(props) {
 
- 
-  
+
+
 
   return (
     <Modal
@@ -28,44 +28,46 @@ function MyVerticallyCenteredModal(props) {
       </Modal.Header>
 
       <Modal.Body >
+
+        <h5>your coins <span style={{color:'gold'}} > <FaCoins /> 150 </span> </h5>
+        <br></br>
+
         <p> progress on product </p>
-        <Progress numberOfUpload = "1" maxUpload = "5" />
+        <Progress numberOfUpload="1" maxUpload="5" />
         <br />
-        
+
         <br />
-        <h4>Why should you add more Images?</h4>
+        <h4>Why should you add more Images/other atrributes?</h4>
         <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
+        User will have ease to view the multiple images and detailed description that will help to choose better product from best supplier according to rating or incentive as a metric.
         </p>
+        <br></br>
 
+        <Container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }} >
 
-        <Container style={{display:'flex' , flexDirection:'row', justifyContent:'space-evenly' }} >
-          
-          <div style={{display:'flex' , flexDirection:'row' , justifyContent:'space-between' }}>
-          
           <Link to="/addProduct" onClick={props.onHide}>
-          <FaImages style={{margin:'auto'}} />
-          <h5 style={{margin:'auto'}}>Add Images</h5>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <FaImages style={{ margin: 'auto' }} />
+              <h5 style={{ margin: 'auto' }}>Add Images</h5>
+            </div>
           </Link>
-          </div>
-          
-          <div style={{display:'flex' , flexDirection:'row' , justifyContent:'space-between' }}>
-          <FaDna style={{margin:'auto' }} />
-          <h5 style={{margin:'auto'}}>Add more Info</h5>
-          </div>
+          <Link to="/addProduct" onClick={props.onHide}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <FaDna style={{ margin: 'auto' }} />
+              <h5 style={{ margin: 'auto' }}>Add more Info</h5>
+            </div>
+          </Link>
 
         </Container>
-        
+
 
 
       </Modal.Body>
       <Modal.Footer>
         <Link to="/congrats">
-          <Button style={{backgroundColor:'rgb(233, 30, 99)', color:'white'}}>skip</Button>
+          <Button style={{ backgroundColor: 'rgb(233, 30, 99)', color: 'white' }}>skip</Button>
         </Link>
-        
+
       </Modal.Footer>
     </Modal>
   );
@@ -77,7 +79,7 @@ const ReminderModal = () => {
 
   return (
     <>
-      <Button variant=""   onClick={() => setModalShow(true)} style={{marginLeft:'600px', backgroundColor:'rgb(233, 30, 99)', color:'white'}}>
+      <Button variant="" onClick={() => setModalShow(true)} style={{ marginLeft: '600px', backgroundColor: 'rgb(233, 30, 99)', color: 'white' }}>
         continue
       </Button>
 
